fe593ab11d8e5120de55376b040d9b1e9d8dced3
Eureka Baloyi <eureka.baloyi@liquid.tech>
2025-06-13 11:12:55 +0200

    changes added

--- src/Navigation.js
+++ src/Navigation.js
@@ -7,6 +7,8 @@ import CloseIcon from '@mui/icons-material/Close';
 import HomeIcon from '@mui/icons-material/Home';
 import ListAltIcon from '@mui/icons-material/ListAlt';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import LogoutIcon from '@mui/icons-material/Logout';
+import ReviewsIcon from '@mui/icons-material/Reviews';
 
 export default function Navigation(){
 
@@ -20,17 +22,24 @@ export default function Navigation(){
         setOpen(!open);
     }
 
+    const handleSignOut= () =>{
+
+    }
+
 
     return (
           <nav className='mt-4 row justify-content-end'>
             <div className='navLinks row justify-content-end t-5 position-absolute '>
               {open && (
-                <div className= 'navstuff text-end bg-body-tertiary shadow'>
-                    <CloseIcon onClick={handleClose} />
+                <div className= 'navbar navstuff text-end bg-body-tertiary p-3 shadow'>
+                    <span><CloseIcon onClick={handleClose} /></span>
                     <Link to='/' className='d-block text-start text-start mb-2 mt-5'><HomeIcon/> Home</Link>
                     <Link to='/profile' className='d-block text-start mb-2'><PersonOutlineIcon/>Profile </Link>
                     <Link to='/Bookings' className='d-block text-start mb-2'><ListAltIcon/> Bookings</Link>
-                    <Link to='/Book' className = 'd-block text-start mb-2'><CalendarMonthIcon/>Make a Booking</Link>
+                    <Link to='/Book' className = 'd-block text-start mb-2'><CalendarMonthIcon/> Make a Booking</Link>
+                    <Link to='/reviews' className='d-block text-start mb-2'><ReviewsIcon/> Reviews</Link>
+                    <hr/>
+                    <Link to='/Login' className='text-start'><LogoutIcon onClick={handleSignOut}  /> Sign out</Link>
               </div>)}
               {!open && (<div className='text-end'>
                 <MenuIcon onClick={handleOpen}/>
